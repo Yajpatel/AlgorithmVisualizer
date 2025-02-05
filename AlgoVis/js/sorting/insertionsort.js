f9ff22be42d5ff87da3b8ad7bb520db7c8730f28
Yajpatel <yajpatel3594@gmail.com>
2025-02-06 00:29:04 +0530

    selection and insertion transition done styling remaining

--- AlgoVis/js/sorting/insertionsort.js
+++ AlgoVis/js/sorting/insertionsort.js
@@ -8,8 +8,66 @@ async function insertionsort() {
             array[j-1] = array[j];
             array[j] = temp;
 
+            await swapBars(j-1, j);
+            await new Promise((resolve) => setTimeout(resolve, 1000));  
             j--;
         }
     }
     console.log("Sorted Array:", array);
+}
+
+
+async function swapBars(index1, index2) {
+    // Access bar elements by their DOM references
+    let bar1 = bars[index1];
+    let bar2 = bars[index2];
+
+    // Calculate their current positions 
+    let pos1 = bar1.offsetLeft;
+    let pos2 = bar2.offsetLeft;
+
+    // Apply CSS transitions for smooth swapping
+    bar1.style.transition = "transform 0.5s";
+    bar2.style.transition = "transform 0.5s";
+
+    // Apply the transform to swap positions
+
+    bar1.style.transform = `translateX(${pos2 - pos1}px)`;
+
+    bar2.style.transform = `translateX(${pos1 - pos2}px) translateY(200px)`;
+
+    // Wait for the transition to complete
+    // this time should be more else that swap color will change early
+    // this is for red color to be visible
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // Temporarily disable transitions before DOM manipulation
+    bar1.style.transition = "none";
+    
+
+    // Reset transforms to avoid visual glitches
+    bar1.style.transform = "translateX(0)";
+    bar2.style.transform = "translateX(0) translateY(0)";
+    
+
+    swapDivs(bar1,bar2)
+
+    // Update the bars array
+    let temp = bars[index1];
+    bars[index1] = bars[index2];
+    bars[index2] = temp;
+}
+
+
+function swapDivs(id1, id2) {
+    let div1 = id1;
+    let div2 = id2;
+
+    if (div1 && div2) {
+        let parent = div1.parentNode;
+        let sibling = div2.nextSibling === div1 ? div2 : div2.nextSibling;
+
+        parent.insertBefore(div2, div1);
+        parent.insertBefore(div1, sibling);
+    }
 }
--- AlgoVis/js/sorting/selectionsort.js
+++ AlgoVis/js/sorting/selectionsort.js
@@ -13,7 +13,7 @@ async function selectionsort(){
         array[i] = temp;
 
         // Swap bars visually
-        await swapBars(i, min);
+        await swapbars(i, min);
         await new Promise((resolve) => setTimeout(resolve, 1000));  
     }
     console.log(array);
@@ -21,40 +21,54 @@ async function selectionsort(){
 }
 
 
-async function swapBars(index1, index2) {
+async function swapbars(index1, index2) {
+    // Access bar elements by their DOM references
     let bar1 = bars[index1];
     let bar2 = bars[index2];
 
+    // Calculate their current positions 
     let pos1 = bar1.offsetLeft;
     let pos2 = bar2.offsetLeft;
 
-    // Apply smooth transitions
+    // Apply CSS transitions for smooth swapping
     bar1.style.transition = "transform 0.5s";
     bar2.style.transition = "transform 0.5s";
 
-    // Move bars
+    // Apply the transform to swap positions
     bar1.style.transform = `translateX(${pos2 - pos1}px)`;
     bar2.style.transform = `translateX(${pos1 - pos2}px)`;
 
-    // Wait for animation
+    // Wait for the transition to complete
+    // this time should be more else that swap color will change early
+    // this is for red color to be visible
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    // Reset transformations
-    // bar1.style.transition = "none";
-    // bar2.style.transition = "none";
+    // Temporarily disable transitions before DOM manipulation
+    bar1.style.transition = "none";
+    bar2.style.transition = "none";
+
+    // Reset transforms to avoid visual glitches
     bar1.style.transform = "translateX(0)";
     bar2.style.transform = "translateX(0)";
 
-    // Swap them in the DOM
-    let parent = bar1.parentNode;
-    if (bar1.nextSibling === bar2) {
-        parent.insertBefore(bar2, bar1);
-    } else {
-        parent.insertBefore(bar1, bar2.nextSibling);
-    }
+    swapDivs(bar1,bar2)
 
     // Update the bars array
     let temp = bars[index1];
     bars[index1] = bars[index2];
     bars[index2] = temp;
+}
+
+
+function swapDivs(id1, id2) {
+    let div1 = id1;
+    let div2 = id2;
+
+    if (div1 && div2) {
+        let parent = div1.parentNode;
+        let sibling = div2.nextSibling === div1 ? div2 : div2.nextSibling;
+
+        parent.insertBefore(div2, div1);
+        parent.insertBefore(div1, sibling);
+    }
 }
